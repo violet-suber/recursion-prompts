a27a4ff516da329a4fe02561423c1d266a0c4a5c
Violet Suber <violetmands@gmail.com>
2017-08-02 14:57:00 -0500

    "Fix Sum of Integers"

--- src/recursion.js
+++ src/recursion.js
@@ -17,9 +17,10 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
-  if (array.length === 0) {
+  var copy = array.slice(0);
+  if (copy.length === 0) {
     return 0;
-  } return array[0] += sum(array.slice(1));
+  } return copy[0] += sum(copy.slice(1));
 };
 
 // *** 3. Sum all numbers in an array containing nested arrays.
